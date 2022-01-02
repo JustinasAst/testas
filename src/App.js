62a6ae477ca38aa186d82d403cdbe72796c3fd19
Justinas Astrauskas <jst.astrauskas@gmail.com>
2022-01-02 13:53:18 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState, Component } from 'react';
+import reactDOM from 'react-dom';
+
+const languages = ['JavaScript', 'Python', 'Ruby', 'JAVA'];
+const languageContext = React.createContext();
+
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			languages,
+			lanIndex: 0,
+		};
+	}
+
+	toggleIndex = () => {
+		this.setState((state) => ({
+			lanIndex: state.languages.length > state.lanIndex + 1 ? state.lanIndex + 1 : 0,
+		}));
+	};
+
+	render() {
+		return (
+			<>
+				<languageContext.Provider
+					value={{ language: this.state.languages[this.state.lanIndex], toggleIndex: this.toggleIndex }}
+				>
+					<MainSection />
+				</languageContext.Provider>
+			</>
+		);
+	}
+}
+
+class MainSection extends React.Component {
+	static contextType = languageContext;
+
+	render() {
+		console.log(this.context);
+		const changeLanguage = () => {
+			this.context.toggleIndex();
+		};
+
+		return (
+			<div>
+				<p id='favoriteLanguage'>Favorite programing languoge: {this.context.language}</p>
+
+				<button id='changeFavorite' onClick={changeLanguage}>
+					Togle language
+				</button>
+			</div>
+		);
+	}
+}
